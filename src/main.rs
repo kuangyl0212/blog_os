5859884f195dc2e71fd27c27bf73c4dcc1bbe0ac
Yulin Kuang <zhh82@zhihaihe.cn>
2023-10-12 13:17:18 +0800

    test: 新增测试框架

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 use core::panic::PanicInfo;
 
@@ -9,14 +20,21 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trival assertion... ");
+    assert_eq!(1, 1);
+    println!("[OK]");
 }
 
 mod vga_buffer;
